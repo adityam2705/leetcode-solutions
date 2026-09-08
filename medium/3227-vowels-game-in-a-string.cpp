1ab087bd8027a96075dc1703904ca6cde6f4d29c
adityam2705 <adityamahara08@gmail.com>
2026-09-08 15:24:07 +0530

    Add LeetCode 3227

---
+++ medium/3227-vowels-game-in-a-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) { 
+        int count=0;
+        for(int i=0; i<s.length(); i++) { 
+    if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u') {
+        count++;
+        break;
+    }
+}
+return(count>0)? true: false;
+        
+    }
+};
