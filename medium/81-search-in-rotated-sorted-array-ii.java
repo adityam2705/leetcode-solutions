f28752a5cf75787227a565f387dd0ba2222bbee1
adityam2705 <adityamahara08@gmail.com>
2026-09-11 23:52:11 +0530

    Delete medium directory

--- medium/81-search-in-rotated-sorted-array-ii.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public boolean search(int[] nums, int target) {
-        for(int i=0,j=nums.length-1 ;i<nums.length;i++,j--){
-            if(target==nums[i]|| target==nums[j]){
-                return true;
-            }
-        }
-   return false; }
-}
--- medium/877-stone-game.java
+++
@@ -1,6 +0,0 @@
-class Solution {
-    public boolean stoneGame(int[] piles) {
-        
-        return true;
-    }
-}
