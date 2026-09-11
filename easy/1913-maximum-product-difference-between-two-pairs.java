7352c458a9a05a7712ed46cf76ee44c257d10527
adityam2705 <adityamahara08@gmail.com>
2026-09-11 23:51:23 +0530

    Delete easy directory

--- easy/1913-maximum-product-difference-between-two-pairs.java
+++
@@ -1,8 +0,0 @@
-class Solution {
-    public int maxProductDifference(int[] nums) {
-        int j= nums.length-1;
-        int i=0;
-        Arrays. sort(nums);
-return (nums[j]*nums[j-1]-nums[i]*nums[i+1]);
-    }
-}
--- easy/217-contains-duplicate.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        for(int i=0;i<nums.length;i++){
-            if(!set.add(nums[i])){return true;}
-        }
-        return false;
-    }
-}
