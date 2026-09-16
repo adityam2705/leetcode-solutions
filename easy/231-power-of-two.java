3d899095a813a8e8dbbe2e673f66e6cbef5ba905
adityam2705 <adityamahara08@gmail.com>
2026-09-16 23:18:20 +0530

    Sync LeetCode solutions

---
+++ easy/231-power-of-two.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+
+    while(n>0){
+        if(n==1)return true;
+        if(n%2==1)return false;
+        n=n/2;}
+ 
+   return false;
+    }
+}
